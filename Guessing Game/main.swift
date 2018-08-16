58a303214313834329a2e72546a2759499c9d68b
Alex Richardson <alexrichardson@macbook-pro-69.local>
2018-08-16 10:23:06 -0500

    Initial Commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Alex Richardson on 8/16/18.
+//  Copyright © 2018 Alex Richardson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
